9164efb555a1cb314ab383602b5ff2a335383712
Suraj Thapa <stsurajst@gmail.com>
2022-01-03 22:06:57 +0545

    book now modal

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./App.css";
+import SearchAppBar from "./components/AppBar";
+
+import Home from "./pages/Home";
+import Tour from "./pages/Tour";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <SearchAppBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<Tour />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/Tour.js
@@ -0,0 +1,59 @@
+import {
+  Container,
+  Typography,
+  Box,
+  Paper,
+  BottomNavigation,
+  BottomNavigationAction,
+} from "@mui/material";
+import CustomizedAccordions from "../components/Accordian";
+import ImageCollage from "../components/ImageCollage";
+import BasicModal from "../components/Modal";
+
+const Tour = () => (
+  <Container sx={{ width: 900 }}>
+    <Typography variant="h3" component="h1" marginTop={3}>
+      Explore the world in Vegas
+    </Typography>
+    <Box marginTop={3} sx={{ display: "flex" }}>
+      <img
+        src="https://media.timeout.com/images/105124791/750/422/image.jpg"
+        alt=""
+        height={325}
+      />
+      <ImageCollage />
+    </Box>
+    <Box>
+      <Typography variant="h6" component="h4" marginTop={3}>
+        About This Ticket
+      </Typography>
+      <Typography variant="paragraph" component="p" marginTop={3}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+      </Typography>
+    </Box>
+    <Box>
+      <Typography variant="h6" component="h4" marginTop={3} marginBottom={2}>
+        Frequently Asked Questions
+      </Typography>
+      <CustomizedAccordions />
+    </Box>
+    <Paper
+      sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
+      elevation={3}
+    >
+      <BottomNavigation>
+        <BasicModal />
+      </BottomNavigation>
+    </Paper>
+  </Container>
+);
+
+export default Tour;
